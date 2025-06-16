ea2b8cda1fc779e56451565567e17ad3657b0a1d
Jonas Lang <jonas@alur.de>
2025-06-16 21:35:32 +0200

    measurements completed

--- figures/duration_coparison.py
+++ figures/duration_coparison.py
@@ -6,19 +6,19 @@ services = ['Instanz-Management', 'Regel', 'Reporting', 'Proxy', 'Nutzeroberflä
 
 # Messdaten (10 Messungen je Service & Testtyp)
 contract_measurements = [
-    [83, 84, 77, 79, 80, 79, 84, 80, 82, 82],  # Instanz-Management
-    [15, 15, 15, 15, 15, 15, 15, 15, 15, 15],  # Regel
-    [9, 9, 9, 9, 9, 9, 9, 9, 9, 9],           # Reporting
-    [14, 14, 14, 14, 14, 14, 14, 14, 14, 14], # Proxy
-    [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]  # Nutzeroberfläche
+    [72, 81, 77, 73, 74, 79, 73, 80, 82, 82],  # Instanz-Management
+    [44, 41, 34, 31, 30, 33, 39, 31, 32, 31],  # Regel
+    [44, 43, 38, 38, 37, 38, 38, 40, 34, 38],  # Reporting
+    [48, 38, 39, 47, 42, 42, 41, 43, 45, 44],  # Proxy
+    [22, 25, 22, 23, 24, 20, 21, 23, 21, 22]   # Nutzeroberfläche
 ]
 
 integration_measurements = [
     [258, 260, 255, 248, 250, 244, 250, 259, 249, 243],  # Instanz-Management
-    [25, 25, 25, 25, 25, 25, 25, 25, 25, 25],  # Regel
-    [18, 18, 18, 18, 18, 18, 18, 18, 18, 18],  # Reporting
-    [22, 22, 22, 22, 22, 22, 22, 22, 22, 22],  # Proxy
-    [19, 19, 19, 19, 19, 19, 19, 19, 19, 19]   # Nutzeroberfläche
+    [ 54,  48,  49,  49,  46,  50,  58,  47,  51,  56],  # Regel
+    [ 84,  74,  67,  67,  63,  68,  69,  69,  67,  65],  # Reporting
+    [284, 274, 291, 288, 289, 304, 293, 301, 300, 314],  # Proxy
+    [234, 285, 266, 254, 251, 257, 262, 267, 249, 261]   # Nutzeroberfläche
 ]
 
 # Durchschnitt berechnen und runden (auf ganze Zahlen)
